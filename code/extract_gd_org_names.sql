5062b927d7eead1c63289749697261bff07714b3
Matthew Corritore <mcorrito@cedar1.cedar.computecanada.ca>
2020-06-16 17:38:01 -0700

    regular update

--- code/extract_gd_org_names.sql
+++ code/extract_gd_org_names.sql
@@ -1,2 +1,7 @@
 --extract gd org names (and compustat names) for merge w/HH
-\copy (SELECT employerid, year, employername, conml FROM firm_chars WHERE nr_500_f_annual_new >= 25 AND NULLIF(employername,'') IS NOT NULL) to '~/projects/def-mcorrito/mcorrito/HH/temp_data/gd_org_names.csv' with csv header;
+--\copy (SELECT employerid, year, employername, conml, gvkey FROM firm_chars WHERE nr_500_f_annual_new >= 25 AND NULLIF(employername,'') IS NOT NULL) to '~/projects/def-mcorrito/mcorrito/HH/temp_data/gd_org_names.csv' with csv header;
+
+\copy (SELECT * FROM firm_chars) to '~/projects/def-mcorrito/mcorrito/HH/temp_data/gd_firm_chars.csv' with csv header;
+
+
+
